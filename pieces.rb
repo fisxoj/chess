73140ff34ee128c142ebd6290dd8872432ac9306
App Academy Student <student@appacademy.io>
2014-09-17 19:07:26 -0400

    Castling logic included (not moves)

--- pieces.rb
+++ pieces.rb
@@ -103,6 +103,81 @@ class King < SteppingPiece
     [[1, 1], [-1, -1], [1, -1], [-1, 1],
      [1, 0], [0, 1], [-1, 0], [0, -1]]
   end
+
+  # def moves(coordinates)
+ #    moves = super
+ #
+ #    ##moves << [castle queen] if self.can_castle?(:queenside)
+ #    ##moves << [castle king]  if self.can_castle?(:kingside)
+ #  end
+
+  def can_castle?(direction)
+    return false unless self.first_move
+    return false unless rook_can_castle?(direction)
+    return false if board.in_check?(self.color)
+    return false unless clear_passage_towards?(direction)
+    return false unless safe_passage_towards?(direction)
+
+    true
+  end
+
+  def find_king_path_squares(direction)
+    if direction == :kingside
+      if self.color == :white
+        [[7, 5], [7, 6]]
+      else
+        [[0, 5], [0, 6]]
+      end
+    else
+      if self.color = :white
+        [[7, 3], [7, 2]]
+      else
+        [[0, 3], [0, 2]]
+      end
+    end
+  end
+
+  def find_rook_path_squares(direction)
+    if direction == :kingside
+      if self.color == :white
+        [[7, 5], [7, 6]]
+      else
+        [[0, 5], [0, 6]]
+      end
+    else
+      if self.color = :white
+        [[7, 3], [7, 2], [7, 1]]
+      else
+        [[0, 3], [0, 2], [0, 1]]
+      end
+    end
+  end
+
+  def rook_can_castle?(direction)
+    row = self.color == :white ? 7 : 0
+    col = direction == :kingside ? 7 : 0
+
+    possible_rook = board[[row, col]]
+
+    possible_rook.class == Rook && possible_rook.first_move
+  end
+
+  def safe_passage_towards?(direction)
+    row = self.color == :white ? 7 : 0
+    col = 4
+    king_pos = [row, col]
+
+    find_king_path_squares(direction).none? do |coordinates|
+      board.leaves_king_in_check?(king_pos, coordinates, self.color)
+    end
+  end
+
+  def clear_passage_towards?(direction)
+    find_rook_path_squares(direction).none? do |coordinates|
+      board.anyone_at?(coordinates)
+    end
+  end
+
 end
 
 class Queen < SlidingPiece
